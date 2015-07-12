fe448ac4540d7a2dbda848a8427bc865100f59b4
d.sharma <d.sharma@99acres.com>
2015-07-12 16:17:54 +0530

    added code

---
+++ MProjet/app/src/main/java/app/rockkworld/com/mproject/volley/MGetGsonRequest.java
@@ -0,0 +1,13 @@
+package app.rockkworld.com.mproject.volley;
+
+import com.android.volley.Response;
+
+/**
+ * Created by divya on 6/7/15.
+ */
+public class MGetGsonRequest extends MRequest {
+
+    public MGetGsonRequest(String url, Response.ErrorListener listener) {
+        super(Method.GET,url, listener);
+    }
+}
---
+++ MProjet/app/src/main/java/app/rockkworld/com/mproject/volley/MPutGsonRequest.java
@@ -0,0 +1,12 @@
+package app.rockkworld.com.mproject.volley;
+
+import com.android.volley.Response;
+
+/**
+ * Created by divya on 6/7/15.
+ */
+public class MPutGsonRequest extends MRequest {
+    public MPutGsonRequest(String url, Response.ErrorListener listener) {
+        super(Method.PUT,url, listener);
+    }
+}
