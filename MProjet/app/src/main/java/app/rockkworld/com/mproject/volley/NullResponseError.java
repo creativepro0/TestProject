5dd0dafb68ea9ad8f5ff0239511a41f89d116511
d.sharma <d.sharma@99acres.com>
2015-07-12 15:46:28 +0530

    added code

---
+++ MProjet/app/src/main/java/app/rockkworld/com/mproject/volley/NullResponseError.java
@@ -0,0 +1,7 @@
+package app.rockkworld.com.mproject.volley;
+
+/**
+ * Created by divya on 8/7/15.
+ */
+public class NullResponseError extends com.android.volley.VolleyError {
+}
---
+++ MProjet/app/src/main/java/app/rockkworld/com/mproject/volley/ResponseMetadata.java
@@ -0,0 +1,9 @@
+package app.rockkworld.com.mproject.volley;
+
+import java.io.Serializable;
+
+/**
+ * Created by divya on 7/7/15.
+ */
+public class ResponseMetadata implements Serializable {
+}
