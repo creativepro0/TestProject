966282974d85ffa4d269afdba4d7c93f7c1d4a7c
rohit-mobicules <rohit.bansal@mobicules.com>
2015-07-16 02:31:49 +0530

    add splash screen with its resources

--- MProjet/app/src/main/java/app/rockkworld/com/mproject/activities/RSplashActivity.java
+++ MProjet/app/src/main/java/app/rockkworld/com/mproject/activities/RSplashActivity.java
@@ -19,49 +19,4 @@ public class RSplashActivity extends Activity {
     }
 
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_rsplash, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
-
-    public void startAnim(View v){
-        Animation bounceAnim=null;
-
-//        ValueAnimator fadeAnim = ObjectAnimator.ofFloat(newBall, "alpha", 1f, 0f);
-//        fadeAnim.setDuration(250);
-//        fadeAnim.addListener(new AnimatorListenerAdapter() {
-//            public void onAnimationEnd(Animator animation) {
-//                balls.remove(((ObjectAnimator)animation).getTarget());
-//            }
-//
-
-//        AnimatorSet bouncer = new AnimatorSet();
-//        bouncer.play(bounceAnim).before(squashAnim1);
-//        bouncer.play(squashAnim1).with(squashAnim2);
-//        bouncer.play(squashAnim1).with(stretchAnim1);
-//        bouncer.play(squashAnim1).with(stretchAnim2);
-//        bouncer.play(bounceBackAnim).after(stretchAnim2);
-//        ValueAnimator fadeAnim = ObjectAnimator.ofFloat(newBall, "alpha", 1f, 0f);
-//        fadeAnim.setDuration(250);
-//        AnimatorSet animatorSet = new AnimatorSet();
-//        animatorSet.play(bouncer).before(fadeAnim);
-//        animatorSet.start();
-    }
 }
